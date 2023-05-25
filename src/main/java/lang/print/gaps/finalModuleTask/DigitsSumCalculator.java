c8a2f462dc7ce8c7da09090578f2fdaf83b661db
Iskender <mederov.iskendr@gmail.com>
2023-05-25 15:52:07 +0600

    first commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number!=0){
+            int digit = number % 10;
+            sum = sum +digit;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
+        boolean print = number > 0;
+        System.out.println(print);
 
     }
 }
